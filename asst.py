f22346bec507d843223ca6cc4c0d29c9b53752ca
Jerry Lam <jerrylam1111@gmail.com>
2023-03-30 16:04:20 +1100

    first commit

---
+++ asst.py
@@ -0,0 +1,11 @@
+import sys
+import secrets
+import time
+
+def generate_ephid():
+    return secrets.token_hex(32)
+
+while True:
+    ephid = generate_ephid()
+    print("Ephemeral ID: " + ephid)
+    time.sleep(15)
