be34b1a84646a970f86c0104aaa8c8330c222cfc
Jerry Lam <jerrylam1111@gmail.com>
2023-03-30 17:06:55 +1100

    task 2

--- asst.py
+++ asst.py
@@ -1,10 +1,13 @@
 import sys
 import secrets
 import time
-
+import Crypto.Protocol.SecretSharing as Shamir
 
 def generate_ephid():
     return secrets.token_hex(32)
+
+def gernerate_shares(ephid, n, k):
+    return Shamir.split(k,n,ephid)
 
 while True:
     ephid = generate_ephid()
