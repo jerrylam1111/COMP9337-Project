142cc9ba4bfa6bee36dbd26e61c70bec344c8d71
Jerry Lam <jerrylam1111@gmail.com>
2023-03-30 16:34:58 +1100

    task 1 completed

--- asst.py
+++ asst.py
@@ -2,10 +2,12 @@ import sys
 import secrets
 import time
 
+
 def generate_ephid():
     return secrets.token_hex(32)
 
 while True:
     ephid = generate_ephid()
     print("Ephemeral ID: " + ephid)
+
     time.sleep(15)
