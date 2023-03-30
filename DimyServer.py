000938bfdb47144774e2183d44f511f8db2c8ddd
Jerry Lam <jerrylam1111@gmail.com>
2023-03-30 18:31:46 +1100

    file name changes

---
+++ DimyServer.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 9999
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a UDP socket
+sock.bind((UDP_IP, UDP_PORT))  # Bind the socket to the specified IP and port
+
+print("Listening for UDP packets on {}:{}".format(UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)  # Receive up to 1024 bytes from the client
+    print("Received message: {} from {}".format(data.decode(), addr))
