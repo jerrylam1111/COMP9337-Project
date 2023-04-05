4fdf692097010b2b298098203968c1d80256fe9c
Jerry Lam <jerrylam1111@gmail.com>
2023-04-05 14:54:22 +1000

    fixed public key hash

--- Dimy.py
+++ Dimy.py
@@ -3,40 +3,60 @@ import secrets
 import time
 import socket
 import random
+import hashlib
 from Crypto.Protocol.SecretSharing import Shamir
 from Crypto.Random import get_random_bytes
 
-def generate_ephid():
-    return get_random_bytes(16)
+public_key = b"""
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1Oi1vEfh9qdKQ2oPRIdy0Yb98I5F
+a3e5ao5Z5z5jRvVVX9C5Ev4dd4y4PVtJtnwR1DpafA5rLlb07oZPQvJ7ZA==
+-----END PUBLIC KEY-----
+"""
 
-def generate_shares(ephid, n, k):
-    shares = Shamir.split(k,n,ephid)
+def generate_ephid(public_key):
+    hash_object = hashlib.sha256(public_key)
+    ephid = hash_object.digest().hex()
+    return ephid[:16]
+
+def generate_shares(ephid, k, n):
+    shares = Shamir.split(k,n,ephid.encode())
     return shares
+
+def hash_message_md5(message):
+    # Create an MD5 hash object
+    md5_hasher = hashlib.md5()
+    # Encode the message as bytes and update the hash object with the message
+    md5_hasher.update(message.encode('utf-8'))
+    # Return the hexadecimal representation of the hashed message
+    return md5_hasher.hexdigest()
 
 while True:
     BROADCAST_IP = "0.0.0.0"
     UDP_PORT = 9999
     listeningADDR = (BROADCAST_IP, UDP_PORT)
 
-    ephid = generate_ephid()
-    print("Ephemeral ID: " + str(ephid))
-    time.sleep(1)
+    ephid = generate_ephid(public_key)
+    print(ephid)
+    time.sleep(15)
 
     k = 3
     n = 5
     shares = generate_shares(ephid, n, k)
+    print(shares)
     randomNo = random.random()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a UDP socket
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # Enable broadcasting
-    #sock.bind(listeningADDR)
+    #sock.bind(listeningADDR)  
     shares_value = str()
+    hashed = hash_message_md5(str(shares_value))
 
     for share in shares:
         shares_value += str(share) + " "
     
     if randomNo < 0.5:
-        print(randomNo)
         sock.sendto(shares_value.encode(), (listeningADDR))
+        sock.sendto(hashed.encode(), (listeningADDR))
         sock.close()
 
     time.sleep(3)
--- DimyServer.py
+++ DimyServer.py
@@ -1,13 +1,24 @@
 import socket
+import hashlib
+
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 9999
 
+public_key = b"""
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1Oi1vEfh9qdKQ2oPRIdy0Yb98I5F
+a3e5ao5Z5z5jRvVVX9C5Ev4dd4y4PVtJtnwR1DpafA5rLlb07oZPQvJ7ZA==
+-----END PUBLIC KEY-----
+"""
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a UDP socket
 sock.bind((UDP_IP, UDP_PORT))  # Bind the socket to the specified IP and port
+
+
 
 print("Listening for UDP packets on {}:{}".format(UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024)  # Receive up to 1024 bytes from the client
-    print("Received message: {} from {}".format(data.decode(), addr))
+    print(data.decode())
