e4733d0ff9a091ed6f6cbbc26a54cf5ae82f0cc8
Hei Lok Keith Kong <z5332481@ad.unsw.edu.au>
2023-04-14 16:35:59 +1000

    a

--- DimyServer.py
+++ DimyServer.py
@@ -7,6 +7,7 @@ UDP_IP = "0.0.0.0"
 UDP_PORT = 9999
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Create a UDP socket
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((UDP_IP, UDP_PORT))  # Bind the socket to the specified IP and port
 
 print("Listening for UDP packets on {}:{}".format(UDP_IP, UDP_PORT))
